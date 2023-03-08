01a82154846f265097d0d488f9bfb87bcd27d216
lumi <lucijamicic154@gmail.com>
2023-03-08 22:27:37 +0100

    init

---
+++ RentClothes.Api/rentclothes/src/main/java/com/example/rentclothes/RentclothesApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentclothesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentclothesApplication.class, args);
+    }
+
+}
---
+++ RentClothes.Api/rentclothes/src/test/java/com/example/rentclothes/RentclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentclothesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
